8673433703540aed847f1822a915cb1372e00304
Juan Pablo Bedoya Mosquera <bedojuanpablo@gmail.com>
2022-03-24 19:25:48 -0500

    24/03/2022 - 2

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -9,7 +9,9 @@ const Layout = () => {
   return (
     <div className='md:flex md:min-h-screen'>
       <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
-        <h2 className='text-4xl font-black text-center text-white'>CRM - Clientes</h2>
+        <h2 className='text-4xl font-black text-center text-white'>
+          <Link to="/">CRM - Clientes</Link>
+        </h2>
         <nav className='mt-10'>
 
           <Link className={`${urlActual === '/clientes' ? 'text-blue-300': 'text-white'} text-2xl block mt-2 hover:text-blue-300`} to="/clientes">Clientes</Link>
